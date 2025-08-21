c4900e1d28176b3496aa8802d0e1355d1f018934
Zumbala <h.petersen@hccnet.nl>
2025-08-21 22:58:14 +0200

    Changes to documentation for Invoice Response

--- .postasciidoctor.sh
+++ .postasciidoctor.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-mv /target/guides/* /target/site/
+if [ -d "/target/site" ]; then
+    mv /target/guides/* /target/site
+else
+    mkdir -p /target/site
+    mv /target/guides/* /target/site
+fi
 rm -r /target/guides
-
